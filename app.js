a2f7d305c752aa7cf4f712d4677967a9b3fef704
Vivek Kumar Saurabh <101538942+vivekkumarsaurabh@users.noreply.github.com>
2023-02-02 15:48:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express= require("express");
 const app=express();
 const path = require("path");
+const ejs = require('ejs');
 
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"./frontend"))
+app.use(express.urlencoded({extended:false}))
 app.get("/",(req,res)=>{
-res.sendFile(path.join(__dirname,"./frontend/index.html"));
+
+res.render("index");
 });
 
+app.get("/contact",(req,res)=>{
+    res.render("contact");
+    });
+    
 
-app.listen(5000);
+
+app.listen(5500);
+
+
+
