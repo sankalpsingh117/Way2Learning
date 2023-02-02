66b9043cdd112c190d7da1f10206ca503c287803
Sankalp Singh <82415171+sankalpsingh117@users.noreply.github.com>
2023-02-02 15:57:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express= require("express");
 const app=express();
 const path = require("path");
 const ejs = require('ejs');
+const user = require('/models/user')
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"./frontend"))
+app.set("views", path.join(__dirname,"/frontend"))
 app.use(express.urlencoded({extended:false}))
 app.get("/",(req,res)=>{
 
@@ -14,6 +15,22 @@ res.render("index");
 app.get("/contact",(req,res)=>{
     res.render("contact");
     });
+
+app.post("/contact",(req,res)=>{
+    InsertRecord(req,res);
+   })  
+
+function InsertRecord(req,res){
+ var users=new User(req.body);
+
+ users.save((err,doc)=>{
+    if(!err){
+        
+        res.redirect('/index');
+    }else{
+        res.status(500).throw(error);
+    }
+ })
     
 
 
